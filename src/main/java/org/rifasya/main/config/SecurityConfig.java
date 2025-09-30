a8133777d6e223a9627db043b1e080084311bd02
Jeisson Fernando Leon Avila <jeissonfernandoleonavila@gmail.com>
2025-09-29 23:27:31 -0500

    CorsFix

--- src/main/java/org/rifasya/main/config/SecurityConfig.java
+++ src/main/java/org/rifasya/main/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -19,4 +21,19 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Permite todo
         return http.build();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // aplica a todos los endpoints
+                        .allowedOrigins("*") // permite cualquier origen
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // todos los métodos
+                        .allowedHeaders("*") // permite cualquier header
+                        .allowCredentials(false); // si necesitas cookies o auth, ponlo en true
+            }
+        };
+    }
+
 }
