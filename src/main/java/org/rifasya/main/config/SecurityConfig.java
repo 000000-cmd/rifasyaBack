2a3df9e31d565cbce0c3d789de73799e06e14039
Jeisson Fernando Leon Avila <jeissonfernandoleonavila@gmail.com>
2025-09-29 23:27:31 -0500

    CorsFix

--- src/main/java/org/rifasya/main/config/SecurityConfig.java
+++ src/main/java/org/rifasya/main/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
